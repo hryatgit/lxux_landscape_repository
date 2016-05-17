07882754b934a57cd5fbbad7ccfa495802141593
Harry Hartmann <hry@solaris-it.com>
2016-05-17 18:35:48 -0400

    authRepoSrc changed

--- commonConstats.sh
+++ commonConstats.sh
@@ -1,5 +1,5 @@
 REPORT_DIR=/tmp/REPORTS
-AUTHORIZATION_FILE_DIR=/home/hry/.ssh
+AUTHORIZATION_FILE_DIR=$HOME/.ssh
 REMOTE_AGENT1="createListsAgnt1"
 LOG_DIR=/tmp/LOG
 MAPPER="createPckgInventoryMapper"
